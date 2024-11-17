2fa5bc6ca83f1eea13caf8523d4e2f1732b5f320
labidimayssa <mayssa.labidi.94@gmail.com>
2024-11-17 22:23:11 +0100

    Aggregations

--- src/main/scala/part1recap/ScalaRecap.scala
+++ src/main/scala/part1recap/ScalaRecap.scala
@@ -1,5 +1,136 @@
 package part1recap
 
-object ScalaRecap {
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object ScalaRecap extends App {
+  //values and variables
+  val aBoolean: Boolean = false
+  // expressions
+  val anIfExpression = if (2 > 3) "bigger" else "smaller"
+
+  //instructions vs expressions
+
+  val theUnit = println("Hello, Scala")
+
+  //functions
+  def myFunction(x: Int) = 42
+
+  //OOP
+  class Animal
+
+  class Cat extends Animal
+
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  class Crocodile extends Animal with Carnivore {
+    override def eat(animal: Animal): Unit = println("Crunch!!")
+  }
+
+  //Singleton Pattern : object xxx, by doing this in one line I've defined both the type my Singleton and the single instance that this type can have.
+
+  object MySingleton
+
+  object Carnivore // Companions
+
+  // generics
+  trait MyList[A]
+
+  //method notation
+  val x = 1 + 2
+  val y = 1.+(2)
+
+  // Functional Programming
+  val incrementer: Int => Int = x => x + 1
+  val incremented = incrementer(42)
+
+  println(incremented)
+
+  // map, flatMap, filter
+
+  val procssedlist = List(1, 2, 3).map(incrementer)
+  procssedlist.foreach(elem => println(elem))
+
+  //Pattern Matching
+
+  val unknown: Any = 45
+  val oridinal = unknown match {
+    case 1 => "first"
+    case 2 => "second"
+    case _ => "unknown"
+  }
+  println(oridinal)
+
+
+  // Try-catch
+  try {
+
+    throw new NullPointerException
+  }
+  catch {
+    case _: NullPointerException => "some returned value"
+    case _ => "something else"
+  }
+
+
+  // Future
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val aFuture = Future {
+    // some expensive computation , runs on another thread
+    42
+  }
+  aFuture.onComplete {
+    case Success(meaningOfLife) => println(s"I've found $meaningOfLife")
+    case Failure(ex) => println(s"I have failed :$ex")
+  }
+
+  //Partial functions
+  val aPartialFunction = (x: Int) => x match {
+    case 1 => 48
+    case 8 => 56
+    case _ => 999
+  }
+
+  //Implicits
+  //auto injection by the compiler
+
+  def methodWithImplicitArgument(implicit x: Int) = x + 43
+
+  implicit val implicitInt = 67
+  val implicitCall = methodWithImplicitArgument
+  println(implicitCall)
+
+  // Implicit conversions - implicit defs
+
+  case class Person(name: String) {
+    def greet = s"Hello, my name is $name"
+  }
+
+  implicit def fromStringToPerson(name: String) = Person(name)
+
+  "Bob".greet // fromStringToPerson("Bob").greet
+
+  // implicit conversion -implicit classes
+
+  implicit class Dog(name: String) {
+    def bark = println("Bark!!")
+  }
+
+  "Lassie".bark //The compiler will automatically convert this String into a dog with that content as an argument
+
+  // How the compiler figures out which implicit to inject here into the argument list ??
+  // It is done  by looking into three areas.
+
+  /*
+  1- Local scope  as  implicit val implicitInt = 67 : Explicitly defining an implicit value here in the same scope
+
+  2- Imported Scope as  import scala.concurrent.ExecutionContext.Implicits.global
+
+  3- Companion objects of the type involved in the method call
+   */
 
 }
--- src/main/scala/part2dataframes/Aggregations.scala
+++ src/main/scala/part2dataframes/Aggregations.scala
@@ -1,5 +1,81 @@
 package part2dataframes
 
-object Aggregations {
+import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.functions.{approx_count_distinct, asc, avg, col, count, countDistinct, desc, desc_nulls_last, exp, lit, mean, stddev, sum}
+
+object Aggregations extends App {
+
+  var spark = SparkSession.builder()
+    .appName("Aggregations AND Grouping")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  //counting
+
+  val moviesDB = spark.read
+    .format("json")
+    .option("mode", "DropMalformed")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/movies.json")
+  moviesDB.show(10)
+
+
+  moviesDB.select(count("*"))
+  moviesDB.select(countDistinct(col("Major_Genre"))).show()
+
+  //approximate counting : this function is very useful for Big Data Frames on which you want to do quick data analysis
+  //approx count will not scan the entire dataFrame but rather it will get you an approximate row count
+  moviesDB.select(approx_count_distinct(col("Major_Genre"))).show()
+
+  //min
+  val minRatingDF = moviesDB.select(functions.min(col("IMDB_Rating"))).show()
+  //data Science
+  //standard deviation means how close or or far the different values to the mean
+  // a lower standard deviation will mean that the values are closer to the average and
+  // the higher standard deviation will mean that the values are more spread out over a wider spectrum.
+
+  moviesDB.select(mean(col("IMDB_Rating")), stddev(col("IMDB_Rating")))
+    .show()
+
+  //Grouping
+
+  //count films by genre
+
+  val countByGenre = moviesDB.groupBy(col("Major_genre"))
+    .count().as("total")
+  countByGenre.show()
+
+  val aggregationDF = moviesDB.groupBy(col("Major_genre"))
+    .agg(count("*").as("N_Movies"),
+      avg("IMDB_Rating").as("Avg_IMDB_Rating"))
+    .orderBy(desc("N_Movies"), desc("Avg_IMDB_Rating"))
+    .show()
+
+  /**
+   * 1. Sum up all profits of all movies
+   * 2. how many distinct directors we have in the DF
+   * 3.mean and standard deviation of us gross revenue
+   * avg imdb rating et the average US gross revenue per director
+   */
+
+  val profits = moviesDB.select((col("US_DVD_Sales") + col("US_Gross") + col("Worldwide_Gross")).as("total_Gross"))
+    .agg(sum("total_Gross")).show()
+
+
+  val distinctDirector = moviesDB.where(col("Director").isNotNull).select(col("Director")).distinct().count()
+  println(s"we have $distinctDirector distinct directors  in the DF")
+
+  moviesDB.select(countDistinct(col("Director"))).show()
+
+  val statisticDf = moviesDB
+    .agg(mean("US_Gross"),
+      stddev("US_Gross")).show()
+
+  val imdbDF = moviesDB.groupBy(col("Director"))
+    .agg(avg(col("IMDB_Rating")).as("avg_IMDB_Rating"),
+      avg(col("US_Gross")))
+    .orderBy(desc_nulls_last("avg_IMDB_Rating"))
+    .show()
+
 
 }
