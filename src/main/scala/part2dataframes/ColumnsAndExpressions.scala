22a2e40b1c0ab930e522399a2a4a40b8938d4392
labidimayssa <mayssa.labidi.94@gmail.com>
2024-11-17 22:37:39 +0100

    DataSources

--- src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -1,5 +1,43 @@
 package part2dataframes
 
-object ColumnsAndExpressions {
+import org.apache.spark
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.util.DropMalformedMode
+import org.apache.spark.sql.functions.col
+
+object ColumnsAndExpressions extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DF Columns and Expressions")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+
+  /** Exercices
+   * 1. Read movies DF and select 2 columns  of your choice
+   * 2. sum 3 column Gross and put value on new column named total profit of the movies
+   * 3. select all comedy movies Major_Genre withn imdb rating 6
+   * use as many version as possible
+   *
+   */
+
+  val moviesDF = spark.read
+    .option("option", "DropMalformed")
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  //moviesDF.show()
+
+  moviesDF.select("Title", "Running_Time_min")
+  val moviesGross = moviesDF.withColumn("total_profit", col("Worldwide_Gross") + col("US_Gross") + col("US_DVD_Sales"))
+  moviesGross.show(10)
+  val genre = moviesDF.select("Major_Genre").distinct().show()
+
+  val comedyMovies = moviesDF.filter(col("Major_Genre") === "Comedy" && col("IMDB_Rating") > 6.1)
+  comedyMovies.show(5)
+
+  val comedyMovies2 = moviesDF.filter("Major_Genre = 'Comedy' and IMDB_Rating > 6.1")
+  comedyMovies2.show(5)
+
 
 }
--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -1,5 +1,109 @@
 package part2dataframes
 
-object DataSources {
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
+
+object DataSources  extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DataSources")
+    .config("spark.master","local")
+    .getOrCreate()
+
+
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  val carsDF = spark.read
+    .format("json")
+    .schema(carsSchema) //enforce a schema
+/**  Important
+ * mode = what sparks should do in case it encounters malformed data
+  **/
+    .option("mode","permessive") // mode : DROPMalformed : will ignore faulty rows, permessive (default)
+    //Failfast will throw an exception if we encounter malformed record.
+    .load("src/main/resources/data/cars.json")
+
+  carsDF.show()
+  //carsDFWithOptionMap.show() // /show is an action
+
+  /*
+  Writing DF
+  - format
+  - save mode = overwrite , append , ignore , errorIfExists
+  - path
+   */
+
+  carsDF.write
+    .mode(SaveMode.Append)
+    .save("src/main/ressources/cars_write.json")
+
+  // Reading from   a remote DB
+  val employeesDF = spark.read
+    .format("jdbc")
+    .options(Map(
+      "driver" -> "org.postgresql.Driver",
+      "url" -> "jdbc:postgresql://localhost:5432/rtjvm",
+      "user" -> "docker",
+      "password" -> "docker",
+      "dbtable" -> "public.employees"
+    ))
+    .load()
+
+  //employeesDF.show()
+  /**
+   * Exercice : read the movies DF , then write it as
+   * tab separated values file csv
+   * snappy parquet
+   * write movies DF in the postgre DB on a table called public.movies
+   */
+
+  val moviesDf = spark.read
+    .format("json")
+    .options(Map(
+    "mode"-> "DropMalformed",
+    "path"-> "src/main/resources/data/movies.json",
+    "inferSchema"->"true"))
+    .load()
+
+  //moviesDf.show(3)
+
+  moviesDf.write
+    .mode(SaveMode.Overwrite)
+    .option("header","true")
+    .option("sep","\t")
+    .csv("src/main/resources/data/movies.csv")
+
+
+  moviesDf.write.mode(SaveMode.Ignore)
+    .save("src/main/resources/data/movies.parquet")
+
+
+  val driver = "org.postgresql.Driver"
+  val url ="jdbc:postgresql://localhost:5432/rtjvm"
+  val user = "docker"
+  val password =  "docker"
+
+  moviesDf.write
+    .format("jdbc")
+    .options(Map(
+      "driver" -> driver,
+      "url" -> url,
+      "user" -> user,
+      "password" ->password ))
+    .mode(SaveMode.Ignore)
+    .option("dbtable","public.movies")
+    .save()
+
+
 
 }
